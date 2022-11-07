349449e9ca883d7eb51c12491b760b1fff2e6b08
“ekornilova” <e.u.kornilova@gmail.com>
2022-11-07 23:03:14 +0200

    remove data to src + add dockerfile

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(logger);
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use("/", indexRouter);
 app.use("/books", booksRouter);
